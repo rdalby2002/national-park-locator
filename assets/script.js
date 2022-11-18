1e6d920a9ea7cd151c19b3b61bb20de911fe4959
MitchH10 <mjh10@live.com>
2022-11-17 19:39:18 -0600

    on click functon for search button

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,8 @@
 const API_KEY = "QknvTF8nyaOfZvolB7zyEiWeLS7BcVcFZ1omLTe6";
 var stateDropdownEl = $("#state-dropsdown");
 var searchButtonEl = $("#srchBtn");
+var selectedState = "";
 
-
+searchButtonEl.on('click', function () {
+    
+});
