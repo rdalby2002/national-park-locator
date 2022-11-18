447e0dc764856c661ab4895138f74fc54e8f0449
MitchH10 <mjh10@live.com>
2022-11-17 19:35:32 -0600

    beginning variable adds

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
 // NPS key: QknvTF8nyaOfZvolB7zyEiWeLS7BcVcFZ1omLTe6
+const API_KEY = "QknvTF8nyaOfZvolB7zyEiWeLS7BcVcFZ1omLTe6";
+var stateDropdownEl = $("#state-dropsdown");
+var searchButtonEl = $("#srchBtn");
+
+
