682d77f4c566c38571d9461db2158faa8896d276
MitchH10 <mjh10@live.com>
2022-11-17 21:47:30 -0600

    added fetch for NPS and printing results to results page

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
 // NPS key: QknvTF8nyaOfZvolB7zyEiWeLS7BcVcFZ1omLTe6
-// google key: AIzaSyCNoCGvv588RGUOYyIPoRXZlS1SezWenwkconst API_KEY = "QknvTF8nyaOfZvolB7zyEiWeLS7BcVcFZ1omLTe6";
-var stateDropdownEl = $("#state-dropsdown");
+// google key: AIzaSyCNoCGvv588RGUOYyIPoRXZlS1SezWenwk
+const NPS_API_KEY = "QknvTF8nyaOfZvolB7zyEiWeLS7BcVcFZ1omLTe6";
+const MAPS_API_KEY = "AIzaSyCNoCGvv588RGUOYyIPoRXZlS1SezWenwk";
+var stateDropdownEl = $("#state-dropdown");
 var searchButtonEl = $("#srchBtn");
+var resultsList = $("#results-list");
 var selectedState = "";
+var npsBaseUrl = "https://developer.nps.gov/api/v1";
+var parkList = {};
 
 searchButtonEl.on('click', function () {
-    
-
+    event.preventDefault();
+    selectedState = stateDropdownEl.val();
+    console.log(selectedState);
+    localStorage.setItem("state", selectedState);
+    document.location = "./results.html";  
 });
